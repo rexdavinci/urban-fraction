140f4b824b5e24a533aab69539fa1cbcf9e8a55c
Abdulhafiz Ahmed <abdulhafizahmed01@gmail.com>
2025-05-10 16:39:36 -0400

    remove subscription section

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -247,7 +247,7 @@ export default function Home() {
         </div>
       </section>
       {/* Newsletter Section */}
-      <section className="py-20 bg-black/40">
+      {/* <section className="py-20 bg-black/40">
         <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
           <h2 className="text-4xl font-bold mb-6">
             <span className="bg-gradient-to-r from-purple-400 to-cyan-400 bg-clip-text text-transparent">
@@ -268,7 +268,7 @@ export default function Home() {
             </button>
           </div>
         </div>
-      </section>
+      </section> */}
 
 
     </div>
